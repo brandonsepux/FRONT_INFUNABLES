76b3ff28c7ed34af11fd7aa4bac3e4252915ce66
XIN9 <xin9@MacBook-Air-de-xin9.local>
2019-12-09 23:59:09 -0300

    estructura_ventanas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { StudentComponent } from './components/student/student.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { CalendarsComponent } from './components/calendars/calendars.component';
+import { ComplaintComponent } from './components/complaints/complaint.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    StudentComponent,
+    CoursesComponent,
+    CalendarsComponent,
+    ComplaintComponent
   ],
   imports: [
     BrowserModule,
