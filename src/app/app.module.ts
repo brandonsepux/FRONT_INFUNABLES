8e5a28417fd268e75363b7a1d80fd84294efeb5b
brandonsepux <brandonsepux@gmail.com>
2019-12-10 17:57:39 -0300

    imagenes y login hecho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,17 @@ import { StudentsComponent } from './components/students/students.component';
 import { CoursesComponent } from './components/courses/courses.component';
 import { CalendarsComponent } from './components/calendars/calendars.component';
 import { ComplaintComponent } from './components/complaints/complaint.component';
+import { RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: "courses", component: CoursesComponent},
+  { path: "login", component: LoginComponent},
+  { path: "register", component: RegisterComponent},
+  { path: "home", component: HomeComponent},
+  { path: "students", component: StudentsComponent},
+  { path: "calendars", component: CalendarsComponent},
+  { path: "complaints", component: ComplaintComponent}
+];
 
 @NgModule({
   declarations: [
@@ -23,6 +34,9 @@ import { ComplaintComponent } from './components/complaints/complaint.component'
     ComplaintComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: false}),
     BrowserModule,
     AppRoutingModule
   ],
